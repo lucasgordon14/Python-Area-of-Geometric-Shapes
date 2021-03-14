2512fde221fedb0a69890d38a9e4086bd73b5c6b
lucasgordon14 <51304358+lucasgordon14@users.noreply.github.com>
2021-03-14 19:40:39 -0400

    First Commit
    This is a first commit and upload of all lines of code required to allow the program to run.  This was a program designed for a school homework assignment using Python. The purpose of this project was to develop code to calculate the area of a rectangle, triangle, and circle using user given inputs.

---
+++ ProjectLab1.py
@@ -0,0 +1,23 @@
+name = "Lucas Gordon"
+address = "My Address"
+phoneNum = "(919)333-8035"
+
+print("Your name is",name)
+
+width = int(input("Enter with width: "))
+height = int(input("Enter with height: "))
+area = width * height
+print("The area is", area, "Square units. ")
+
+base = int(input("Enter with base: "))
+height = int(input("Enter with height: "))
+areaTri = .5 * base * height
+print("The area is", areaTri, "Square units. ")
+
+radius =  int(input("Enter with radius: "))
+areaCirc = 3.14 * radius ** 2
+print("The area is", areaCirc, "Square units. ")
+
+userName =  str(input("Enter with user name: "))
+age = int(input("Enter with age: "))
+print("Name: ", userName, "Age: ", age)
